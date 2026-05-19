6b062a296156f1eb66a28f16d4bc8d63d42c35f6
Guruprasad hv <157045040+Guruprasadhv@users.noreply.github.com>
2026-05-20 03:19:08 +0530

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,19 +1,21 @@
 <?php
-// Database configuration
-$host = 'localhost';
-$port = 3306;
-$user = 'root';
-$pass = '';
-$db   = 'student_budget_management';
 
-// Disable mysqli errors
+// Disable MySQL fatal errors
 mysqli_report(MYSQLI_REPORT_OFF);
+
+// Database configuration
+$host = "localhost";
+$port = 3306;
+$user = "root";
+$pass = "";
+$db   = "student_budget_management";
 
 // Create connection
 $conn = @new mysqli($host, $user, $pass, $db, $port);
 
-// Check connection
+// If database fails, stop crashing
 if ($conn->connect_error) {
     $conn = null;
 }
+
 ?>
